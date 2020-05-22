bc5347dc730da87f2ba13dcb1b9f97b9709d0e6e
Kevin Pandura <pandu.kev.kp@gmail.com>
2020-05-23 01:14:51 +0200

    Adding Frames
    Adding Frames with JS

--- js/functions.js
+++ js/functions.js
@@ -2,22 +2,42 @@
 var xOff = window.pageXOffset;
 var totalWidth = window.innerWidth * 2;
 var scrollPercent = 0;
-
-var frames = document.querySelectorAll(".frame");
+var content = document.querySelector(".content");
 
 console.log(xOff);
 console.log(totalWidth);
+
+createFrames(10);
+var frames = document.querySelectorAll(".frame");
 
 window.onscroll = function () {
     xOff = window.pageXOffset;
     percent = (100 * xOff) / totalWidth;
     console.log(percent);
 
-    if (percent <= 50) {
-        frames[0].style.visibility = "visible";
-        frames[1].style.visibility = "hidden";
-    } else {
-        frames[1].style.visibility = "visible";
-        frames[0].style.visibility = "hidden";
-    }
+    var frameId = Math.floor(percent/10);
+    showFrame(frameId);
 };
+
+
+function createFrames(frameCount){
+    var frame;
+    for(var i = 0; i < frameCount; i++){
+        frame = document.createElement("div");
+        frame.setAttribute("frame-id",i);
+        frame.classList.add("frame");
+        frame.innerHTML = i;
+        content.appendChild(frame);
+    }
+}
+
+function showFrame(frameId){
+
+    frames.forEach(element => {
+        if(frameId == element.getAttribute("frame-id")){
+            element.classList.add("visibleFrame100");
+        } else {
+            element.classList.remove("visibleFrame100");
+        }
+    });
+}
